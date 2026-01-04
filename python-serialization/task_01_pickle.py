ab36a69d69b133fea3b12160f60559c2b8c85458
Amrah2609 <emrah.kerimov1111@gmail.com>
2026-01-04 19:16:09 +0400

    task1

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -3,28 +3,18 @@
 
 import pickle
 
+class CustomObject:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __repr__(self):
+        return f"<CustomObject name={self.name} value={self.value}>"
 
 def serialize_and_save_to_file(data, filename):
-    """
-    Serialize a Python object and save it to a file using Pickle.
-
-    Args:
-        data: Any Python object (dict, list, tuple, set, class instance, etc.)
-        filename: Output file name
-    """
-    with open(filename, "wb") as f:  # wb → write binary
+    with open(filename, "wb") as f:
         pickle.dump(data, f)
 
-
 def load_and_deserialize(filename):
-    """
-    Load a Pickle file and deserialize it into a Python object.
-
-    Args:
-        filename: Input Pickle file
-
-    Returns:
-        Python object loaded from the file
-    """
-    with open(filename, "rb") as f:  # rb → read binary
+    with open(filename, "rb") as f:
         return pickle.load(f)
