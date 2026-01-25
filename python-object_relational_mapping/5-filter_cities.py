e664fbe9d0cc29462b702871fbe3590bb0fa3fa8
Amrah2609 <emrah.kerimov1111@gmail.com>
2026-01-25 22:09:24 +0400

    task

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
 """
-List all cities of a state
+First ORM
 """
-import sys
+
 import MySQLdb
+import sys
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.state_id = states.id \
-    WHERE states.name = '{}';".format(sys.argv[4]))
-    states = cur.fetchall()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=database
+    )
 
-    print(", ".join([state[1] for state in states]))
+    cursor = db.cursor()
+
+    cursor.execute(
+        """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """,
+        (state_name,)
+    )
+
+    rows = cursor.fetchall()
+    print(", ".join(row[0] for row in rows))
+
+    cursor.close()
+    db.close()
